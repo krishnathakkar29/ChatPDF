2356e465dd7ee7821f027ea5c6114ed9b299c79c
Krishna Thakkar <work.krishnat@gmail.com>
2024-08-20 02:22:48 +0530

    applying links to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { LogIn } from "lucide-react";
+import { eq } from "drizzle-orm";
+import { ArrowRight, LogIn } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -19,7 +29,13 @@ export default async function Home() {
           </div>
 
           <div className="flex mt-2">
-            {isAuth && <Button>Go To Chats</Button>}
+            {isAuth && (
+              <Link href={`/chat/${firstChat?.id}`}>
+                <Button>
+                  Go to Chats <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
+            )}
           </div>
           <p className="max-w-xl mt-1 text-lg text-slate-600">
             Join millions of students, researchers and professionals to
@@ -28,7 +44,7 @@ export default async function Home() {
 
           <div className="w-full mt-4">
             {isAuth ? (
-              <FileUpload />  
+              <FileUpload />
             ) : (
               <Link href="/sign-in">
                 <Button>
